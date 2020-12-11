f77a38e3f96ded3b1cf2d3c779e2f0f60683c653
Douglas Johnson <trunks22a@gmail.com>
2020-12-10 21:00:56 -0800

    Finished Project

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Password-Generator
+
+## Welcome to your new Password Generator!
+
+I have put together a Password Generator that meets your specifications so that you can be secure when storing your sensitive data. Simply follow the prompts connected to the webpage and retrieve the password from the field in the screen!
+
+Below you will find examples of the code that went into making your password generator. It is designed to keep inputs within the parameters I set and will nudge you in the right direction if you fail to give it the input that it needs.
+
+### This is the code that asks you to input the password length
+![prompt-code](https://i.postimg.cc/KzXFtpFY/pw-1.png)
+
+### This is the code that generates lower case letters if you choose to include them
+![lowercase-code](https://i.postimg.cc/ZRLg0w52/pw-2.png)
+
+### This is the code that makes sure you give the generator a character set
+![edge-case-code](https://i.postimg.cc/s2d0ckBb/pw-3.png)
+
+Thank you for your patronage!
+
+-Douglas Johnson
+
+[Github](https://github.com/DougJohnson22/Password-Generator)
+<br>
+[Live-Site](https://dougjohnson22.github.io/Password-Generator/)
--- script.js
+++ script.js
@@ -1,14 +1,81 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+}
+var finalPassArray = ""
+var finalPass = ""
+var CapLetterArray = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var LowLetterArray = ("abcdefghijklmnopqrstuvwxyz")
+var NumArray = ("1234567890")
+var SpecArray = ("!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+
+//Create a "generate password" function and write all the logic within this function
+
+function generatePassword() {
+
+  //Create promt to ask user how many char they like they pass to be ( at least 8 characters and no more than 128) and set to a variable
+
+  var passLen = prompt("How many characters would you like your password to be? Must be between 8 and 128 characters.");
+  if (isNaN(passLen) || passLen < 8 || passLen > 128) {
+    alert("Please enter a valid length.")
+    var passLen = prompt("How many characters would you like your password to be? Must be between 8 and 128 characters.")
+  }
+
+
+  // Create confirm to ask user if they want capital letters and set to a variable
+  var CapLetters = confirm("Would you like capitial letters in your Password?");
+  if (CapLetters === true) {
+
+    finalPassArray = finalPassArray + CapLetterArray;
+  }
+
+
+  // Create confirm to ask user if they want lowercase letters and set to a variable
+  var LowLetters = confirm("Would you like lower case letters in your Password?");
+  if (LowLetters === true) {
+
+    finalPassArray = finalPassArray + LowLetterArray;
+  }
+
+
+  // Create confirm to ask user if they want numbers and set to a variable
+  var Numbers = confirm("Would you like numbers in your Password?");
+  if (Numbers === true) {
+
+    finalPassArray = finalPassArray + NumArray;
+  }
+
+  // Create confirm to ask user if they want Special char and set to a variable
+  var Special = confirm("Would you like special characters in your Password?");
+  if (Special === true) {
+
+    finalPassArray = finalPassArray + SpecArray;
+  }
+
+
+  // Create a conditional to make sure tha the user has chosen at leat one type of char
+
+  if (CapLetters === false && LowLetters === false && Numbers === false && Special === false) {
+    alert("Please choose one type of character for your array")
+    generatePassword()
+  }
+
+
+  console.log(passLen)
+  console.log(finalPassArray)
+
+  // Create For loop to generate final password with user decisions
+  for (var i = 0; i < passLen; i++) {
+    var randomNum = Math.floor(Math.random() * finalPassArray.length);
+    (finalPass += finalPassArray[randomNum])
+  }
+
+  // Push Final Password!
+  return finalPass;
 }
 
 // Add event listener to generate button
